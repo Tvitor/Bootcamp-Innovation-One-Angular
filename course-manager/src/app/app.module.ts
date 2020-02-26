874ce880f626e228171ff2a39303112025b9bc48
tvitor <contato.vst@gmail.com>
2020-02-26 20:36:39 -0300

    criando rotas

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
@@ -7,18 +8,31 @@ import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/star.component';
 import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-
+import { error404Component } from './404/error-404.component';
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StarComponent,
     ReplacePipe,
-    NavBarComponent
+    NavBarComponent,
+    error404Component
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule, 
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: 'courses',  component: CourseListComponent
+      },
+      {
+        path: '**', component: error404Component
+      }
+    ])
+
     
   ],
   providers: [],
--- course-manager/src/app/courses/course-list.component.ts
+++ course-manager/src/app/courses/course-list.component.ts
@@ -3,7 +3,6 @@ import {Component, OnInit} from '@angular/core';
 import { CourseService } from './course.service';
 
 @Component({
-    selector: 'app-course-list',
     templateUrl: 'course-list.component.html'
 })
 export class CourseListComponent implements OnInit {
