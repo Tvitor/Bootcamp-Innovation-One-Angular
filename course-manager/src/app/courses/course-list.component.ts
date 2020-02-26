5a9a4273d7e16f5b176d19a11ec0aa30d4cbb98e
tvitor <contato.vst@gmail.com>
2020-02-26 19:47:43 -0300

    criando o proprio pipe

--- course-manager/src/app/courses/course-list.component.ts
+++ course-manager/src/app/courses/course-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
     import { Course } from './course';
 import { CourseService } from './course.service';
+
 @Component({
     selector: 'app-course-list',
     templateUrl: 'course-list.component.html'
